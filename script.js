64ab9d94c99c361c9fce24f7c58f7169f24158f8
Andrzej <andrzej.gawera@gmail.com>
2020-01-21 10:55:21 +0100

    Done

---
+++ script.js
@@ -0,0 +1,41 @@
+function drawMirrorTree(n = 5) {
+  for (let i = 0 ; i < n; i++) {
+    let line = "";
+    for (let j = i; j < n ; j++) {
+      line += "*";
+    }
+    console.log(line);
+  }
+}
+
+function drawTree(n = 5) {
+  let line = "";
+  for (let i = 0; i < n; i++) {
+    line += "*";
+    console.log(line);
+  }
+}
+
+function drawMatrix(n = 5) {
+  for (let i = 1 ; i <= n; i++) {
+    let line = "";
+    for (let j = 0; j <= n ; j++) {
+      line += j + " ";
+    }
+    console.log(line);
+  }
+}
+
+function drawChristmasTree(n = 5) {
+  for (let i = 1 ; i < n; i++) {
+    let line = "";
+    for (let k = i; k < n ; k++) {
+      line += " ";
+    }
+    for (let j = 1; j <= (2*i-1) ; j++) {
+      line += "*";
+    }
+    console.log(line);
+  }
+}
+
